285e7f63a693c596575e7518a2b368b95a9324aa
Jason <jjaniga3@luc.edu>
2021-11-01 20:13:17 -0500

    Final

--- INFS791 - Group Project - First Draft - Maria Jason Brian.py
+++ INFS791 - Group Project - First Draft - Maria Jason Brian.py
@@ -297,7 +297,7 @@ x_train, x_test, y_train, y_test = train_test_split(arrive_x, arrive_y,
                                     test_size=0.25, random_state = 30)
 y_train = np.ravel(y_train)
 
-# used the SAGA machine learning solver with LogisticRegression to predict flight delays
+# used the lbfgs machine learning solver with LogisticRegression to predict flight delays for MDW and ORD
 classifier = LogisticRegression(solver='lbfgs', max_iter=10000).fit(x_train, y_train) 
 
 print("\nTraining score of model: ")
